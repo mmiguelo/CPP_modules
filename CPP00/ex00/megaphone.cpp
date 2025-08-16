8d2324d69f81f0957cab781d080e82c107278664
root <root@DESKTOP-V4EO9N9.localdomain>
2025-08-16 17:02:12 +0100

    added ex00 of CPP00

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/16 16:42:14 by mmiguelo          #+#    #+#             */
+/*   Updated: 2025/08/16 16:42:14 by mmiguelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for(int i = 0; av[++i];)
+	{
+		for (int j = -1; av[i][++j];)
+			std::cout << (char)toupper(av[i][j]);
+	}
+	std::cout << "\n";
+}
