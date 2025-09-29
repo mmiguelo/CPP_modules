261d6f971aa15486ab5fe6c3883d2d4a5d6b9137
mmiguelo <marco_miguelote@hotmail.com>
2025-09-29 19:09:58 +0100

    added ex00 but incomplete

---
+++ CPP02/ex00/Fixed.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <cstdlib>
+
+#define RST "\033[0m"      // reset color
+#define R "\033[1;31m"   // bold red
+#define G   "\033[1;32m"   // bold green
+
