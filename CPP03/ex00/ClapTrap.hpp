a8c8f16dee8e78ab2fc5a3bd4a104c4ee8888f85
mmiguelo <marco_miguelote@hotmail.com>
2025-10-08 15:36:34 +0100

    First files of CPP03/ex00

---
+++ CPP03/ex00/ClapTrap.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+
+#define RST		"\033[0m"
+#define RED		"\033[31m"
+#define G		"\033[32m"
+
+class ClapTrap
+{
+	private:
+		std::string	_name;
+		unsigned int	_hitPoints;
+		unsigned int	_energyPoints;
+		unsigned int	_attackDamage;
+	public:
+	// orthodox canonical form:
+		ClapTrap();
+		ClapTrap(const ClapTrap &src);
+		ClapTrap(const std::string name);
+		ClapTrap& operator= (const ClapTrap &src);
+		~ClapTrap();
+
+	// methods:
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+};
