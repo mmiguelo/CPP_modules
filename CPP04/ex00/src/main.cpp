fc1119b1d3ee49301b8e32554e733f14176ec5ab
mmiguelo <marco_miguelote@hotmail.com>
2025-10-16 13:04:41 +0100

    changed main and some files to be ready to submit

--- CPP04/ex00/src/main.cpp
+++ CPP04/ex00/src/main.cpp
@@ -11,16 +11,27 @@ int main()
     const Animal* i = new Cat();
 	const WrongAnimal* l = new WrongAnimal();
 	const WrongAnimal* k = new WrongCat();
-	std::cout << j->getType() << " " << std::endl;
-	j->makeSound();
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
-	std::cout << meta->getType() << " " << std::endl;
-	meta->makeSound();
-	std::cout << l->getType() << " " << std::endl;
-	l->makeSound();
-	std::cout << k->getType() << " " << std::endl;
-	k->makeSound(); //makeSounfd() is not virtual, so it calls the base class method
+
+	{
+		std::cout << j->getType() << " " << std::endl;
+		j->makeSound();
+		std::cout << i->getType() << " " << std::endl;
+		i->makeSound(); //will output the cat sound!
+		std::cout << meta->getType() << " " << std::endl;
+		meta->makeSound();
+		std::cout << l->getType() << " " << std::endl;
+		l->makeSound();
+		std::cout << k->getType() << " " << std::endl;
+		k->makeSound(); //makeSound() is not virtual, so it calls the base class method
+	}
+
+	{
+		delete meta;
+		delete j;
+		delete i;
+		delete l;
+		delete k;
+	}
 
 	return 0;
 }
