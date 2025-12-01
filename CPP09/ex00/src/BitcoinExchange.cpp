a6fc23ebd122c56c3cb7d4afd7702bf31daa8f5a
mmiguelo <marco_miguelote@hotmail.com>
2025-12-01 17:11:59 +0000

    starting with first functions

--- CPP09/ex00/src/BitcoinExchange.cpp
+++ CPP09/ex00/src/BitcoinExchange.cpp
@@ -15,6 +15,29 @@ BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other) {
 	return *this;
 }
 
+bool isValidDate(std::string date)
+{
+	if(date.length() != 10)
+		return false;
+	if (date[4] != '-' || date[7] != '-')
+		return false;
+	for(int i = 0; i < date.length(); i++)
+	{
+		if (i == 4 || i == 7)
+			continue;
+		if (!std::isdigit(date[i]))
+			return false;
+	}
+	int year = std::atoi(date.substr(0, 4).c_str());
+	int month = std::atoi(date.substr(5, 2).c_str());
+	int day = std::atoi(date.substr(8, 2).c_str());
+	if (month < 1 || month > 12)
+		return false;
+	if (day < 1 || day > 31)
+		return false;
+	return true;
+}
+
 void BitcoinExchange::loadData(const std::string& filename)
 {
 	std::ifstream file(filename.c_str());
