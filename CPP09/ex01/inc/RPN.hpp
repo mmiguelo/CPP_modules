42bdaa1c66d2b058f91b0f303ce89142e29386c1
mmiguelo <marco_miguelote@hotmail.com>
2025-12-09 07:20:39 +0000

    some minor adjustments

--- CPP09/ex01/inc/RPN.hpp
+++ CPP09/ex01/inc/RPN.hpp
@@ -9,5 +9,10 @@
 class RPN
 {
 	public:
+		RPN();
+		~RPN();
+		RPN(const RPN& other);
+		RPN& operator=(const RPN& other);
+		int evaluate(const std::string& expression);
 	private:
 }
--- CPP09/ex01/src/RPN.cpp
+++ CPP09/ex01/src/RPN.cpp
@@ -1,3 +1,22 @@
 #include "../inc/RPN.hpp"
 
 RPN::RPN() {}
+RPN::~RPN() {}
+RPN::RPN(const RPN& other) { (void)other; }
+RPN& RPN::operator=(const RPN& other) 
+{ 
+	(void)other; 
+	return *this; 
+}
+
+int RPN::evaluate(const std::string& expression)
+{
+	std::istringstream iss(expression);
+	std::string token;
+
+	std::stack<int> stk;
+	while (iss >> token)
+	{
+		int 
+	}
+}
